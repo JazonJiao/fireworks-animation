2660f046d062070337e6b7cb8da805747070ed3c
Canwen Jiao <canwen.jiao@vanderbilt.edu>
2022-02-12 22:32:00 -0800

    aaa

--- 220214.js
+++ 220214.js
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=CKeyIbT3vXI&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=30
+
 /*** 2022-02-10
  * ----args list parameters----
  * @mandatory (number) x, y, w, h
@@ -43,6 +45,97 @@ class Love extends PointBase {
   }
 }
 
+function rnd(a, b) { return Math.random() * (b - a) + a; }
+
+let G = 0.03;
+
+
+class Star {
+  constructor(ctx, x, y) {
+    this.s = ctx;
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = rnd(-19, -9);
+    this.ax = 0;
+    this.ay = 0;
+
+  }
+
+  update() {
+    this.ay += G;
+    this.vx += this.ax;
+    this.vy += this.ay;
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  show() {
+    this.s.stroke(255, 255, 255);
+    this.s.strokeWeight(3);
+    this.s.point(this.x, this.y);
+  }
+}
+
+
+class Particle {
+  constructor(ctx, x, y) {
+    this.s = ctx;
+    this.x = x;
+    this.y = y;
+    this.timer = new Timer2(100);  //
+    this.vx = rnd(-10, 10);
+    this.vy = rnd(-10, 10);
+    this.ax = 0;
+    this.ay = 0;
+
+  }
+
+  update() {
+    this.ax += 0;
+    this.ay += G;
+    this.vx += this.ax;
+    this.vy += this.ay;
+    this.vx *= 0.95;
+    this.vy *= 0.95;
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  show() {
+    let t = this.timer.advance();
+    if (t > 0) {
+      this.update();
+      this.s.stroke(255 * (1 - t));
+      this.s.strokeWeight(3);
+      this.s.point(this.x, this.y);
+    }
+  }
+}
+
+
+class Firework {
+  constructor(ctx) {
+    this.s = ctx;
+    this.fw = new Star(this.s, rnd(100, 600), ctx.height);
+    this.ps = [];
+    this.exploded = false;
+  }
+
+  show() {
+    if (!this.exploded) {
+      this.fw.show();
+      if (this.fw.vy >= 0) {  // reaches the top
+        this.exploded = true;
+        for (let i = 0; i < 49; i++)
+          this.ps.push(new Particle(this.s, this.fw.x, this.fw.y));
+      }
+    } else {
+      for (let p of this.ps) p.show();
+    }
+  }
+}
+
 
 const Graph01 = function(s) {
   let t = {
@@ -58,6 +151,8 @@ const Graph01 = function(s) {
     s.love = new Love(s, {
       start: t.love, x: 579, y: 199, scale: 9
     });
+    s.fw = [];
+
     s.txt = [];
     s.txt[0] = new TextWriteIn(s, {
       str: "Happy Valentine’s Day to Jessie!", x: 349, y: 437, font: tnr, start: t.txt
@@ -72,6 +167,10 @@ const Graph01 = function(s) {
   };
   s.draw = function () {
     s.background(0);
+    if (Math.random() < 0.1)
+      s.fw.push(new Firework(s));
+    for (let f of s.fw) f.show();
+
     for (let x of s.txt) x.show();
     s.love.show();
     s.d.show();
